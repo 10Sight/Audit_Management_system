6826c25aaab4ad8a2d9c7b9d0d7411d8ef66bb10
Jatin Kumar Nagar <jatinnagar563@gmail.com>
2026-01-16 15:00:40 +0530

    replace mysql2 with mysql

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,11 +1,82 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql";
 import logger from "../logger/winston.logger.js";
 import EVN from "../config/env.config.js";
+
+// Wrapper class to mimic mysql2/promise Connection
+class PromiseConnection {
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+    query(sql, values) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, values, (err, results, fields) => {
+                if (err) return reject(err);
+                resolve([results, fields]);
+            });
+        });
+    }
+
+    release() {
+        this.connection.release();
+    }
+
+    beginTransaction() {
+        return new Promise((resolve, reject) => {
+            this.connection.beginTransaction((err) => {
+                if (err) return reject(err);
+                resolve();
+            });
+        });
+    }
+
+    commit() {
+        return new Promise((resolve, reject) => {
+            this.connection.commit((err) => {
+                if (err) return reject(err);
+                resolve();
+            });
+        });
+    }
+
+    rollback() {
+        return new Promise((resolve, reject) => {
+            this.connection.rollback(() => {
+                resolve();
+            });
+        });
+    }
+}
+
+// Wrapper class to mimic mysql2/promise Pool
+class PromisePool {
+    constructor(pool) {
+        this.pool = pool;
+    }
+
+    query(sql, values) {
+        return new Promise((resolve, reject) => {
+            this.pool.query(sql, values, (err, results, fields) => {
+                if (err) return reject(err);
+                resolve([results, fields]);
+            });
+        });
+    }
+
+    getConnection() {
+        return new Promise((resolve, reject) => {
+            this.pool.getConnection((err, connection) => {
+                if (err) return reject(err);
+                resolve(new PromiseConnection(connection));
+            });
+        });
+    }
+}
 
 let pool;
 
 const createPool = (database) => {
-    pool = mysql.createPool({
+    const rawPool = mysql.createPool({
         host: EVN.MYSQL_HOST,
         user: EVN.MYSQL_USER,
         password: EVN.MYSQL_PASSWORD,
@@ -16,6 +87,7 @@ const createPool = (database) => {
         port: EVN.MYSQL_PORT || 3306,
         charset: 'utf8mb4_general_ci'
     });
+    pool = new PromisePool(rawPool);
 };
 
 const connectDB = async () => {
@@ -23,7 +95,7 @@ const connectDB = async () => {
         const dbName = EVN.MYSQL_DATABASE || "Audit Management System";
 
         // create temporary connection without database to ensure DB exists
-        const tmpConn = await mysql.createConnection({
+        const tmpConn = mysql.createConnection({
             host: EVN.MYSQL_HOST,
             user: EVN.MYSQL_USER,
             password: EVN.MYSQL_PASSWORD,
@@ -31,8 +103,26 @@ const connectDB = async () => {
             charset: 'utf8mb4_general_ci'
         });
 
-        await tmpConn.query(`CREATE DATABASE IF NOT EXISTS \`${dbName}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci`);
-        await tmpConn.end();
+        const queryPromise = (conn, sql) => {
+            return new Promise((resolve, reject) => {
+                conn.query(sql, (err, results) => {
+                    if (err) return reject(err);
+                    resolve(results);
+                });
+            });
+        };
+
+        const endPromise = (conn) => {
+            return new Promise((resolve, reject) => {
+                conn.end((err) => {
+                    if (err) return reject(err);
+                    resolve();
+                });
+            });
+        };
+
+        await queryPromise(tmpConn, `CREATE DATABASE IF NOT EXISTS \`${dbName}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci`);
+        await endPromise(tmpConn);
 
         createPool(dbName);
 
